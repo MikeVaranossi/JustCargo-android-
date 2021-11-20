7981b4909154c1c09446f444d4b27e938e8732f9
Artem <artemisuzl@gmail.com>
2021-11-20 16:22:22 +0300

    create host activity

---
+++ app/src/main/java/com/uzlov/valitova/justcargo/ui/HostActivity.kt
@@ -0,0 +1,11 @@
+package com.uzlov.valitova.justcargo.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HostActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
