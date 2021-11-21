4e31a53bbba3cee3070accbdeda35d6c10700c45
Artem <artemisuzl@gmail.com>
2021-11-21 21:04:08 +0300

    add base fragments

---
+++ app/src/main/java/com/uzlov/valitova/justcargo/ui/fragments/HomeFragment.kt
@@ -0,0 +1,33 @@
+package com.uzlov.valitova.justcargo.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.uzlov.valitova.justcargo.R
+
+class HomeFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    companion object {
+        fun newInstance(param1: String, param2: String) =
+            HomeFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+}
