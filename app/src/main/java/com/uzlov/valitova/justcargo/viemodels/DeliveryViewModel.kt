48922c29a35acd42e2d0e5c32adfae43cb23ca50
Artem <artemisuzl@gmail.com>
2021-11-22 22:03:04 +0300

    add files viewmodels

---
+++ app/src/main/java/com/uzlov/valitova/justcargo/viemodels/DeliveryViewModel.kt
@@ -0,0 +1,10 @@
+package com.uzlov.valitova.justcargo.viemodels
+
+import androidx.lifecycle.ViewModel
+
+class DeliveryViewModel : ViewModel() {
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+}
