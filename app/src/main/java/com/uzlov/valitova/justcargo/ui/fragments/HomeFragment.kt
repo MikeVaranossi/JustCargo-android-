961bfbcecf0686dbde636c98ff12832f9a884c22
Yulia Valitova <ikskulfg@gmail.com>
2021-11-30 19:27:33 +0300

    Стартовый экран - начало

--- app/src/main/java/com/uzlov/valitova/justcargo/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/uzlov/valitova/justcargo/ui/fragments/HomeFragment.kt
@@ -1,33 +1,54 @@
 package com.uzlov.valitova.justcargo.ui.fragments
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 import com.uzlov.valitova.justcargo.R
+import com.uzlov.valitova.justcargo.databinding.FragmentHomeBinding
+import java.util.*
 
 class HomeFragment : Fragment() {
 
+    private var _viewBinding: FragmentHomeBinding? = null
+    private val viewBinding get() = _viewBinding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = FragmentHomeBinding.inflate(layoutInflater, container, false).also {
+        _viewBinding = it
+    }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loadImage(R.drawable.image_home_fragment, viewBinding.imageViewMain )
 
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    override fun onDestroyView() {
+        _viewBinding = null
+        super.onDestroyView()
     }
 
     companion object {
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance() =
             HomeFragment().apply {
                 arguments = Bundle().apply {
 
                 }
             }
     }
+
+    fun loadImage(image: Int, container: ImageView){
+        Glide.with(view!!.context)
+            .load(image)
+            .into(container)
+    }
+
 }
