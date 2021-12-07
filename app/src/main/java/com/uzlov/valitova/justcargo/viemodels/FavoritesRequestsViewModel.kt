07257137c686961a947f43297776eb2df575ac71
Artem <artemisuzl@gmail.com>
2021-12-07 22:22:06 +0300

    Исправил ошибку даггера

--- app/src/main/java/com/uzlov/valitova/justcargo/viemodels/FavoritesRequestsViewModel.kt
+++ app/src/main/java/com/uzlov/valitova/justcargo/viemodels/FavoritesRequestsViewModel.kt
@@ -3,6 +3,6 @@ package com.uzlov.valitova.justcargo.viemodels
 import javax.inject.Inject
 
 // ViewModel для взаимодествия с данными из БД (заявками добавленным ив избранное)
-class FavoritesRequestsViewModel /*@Inject constructor(LocalRepository)*/ : BaseViewModel() {
+class FavoritesRequestsViewModel @Inject constructor() : BaseViewModel() {
 
 }
