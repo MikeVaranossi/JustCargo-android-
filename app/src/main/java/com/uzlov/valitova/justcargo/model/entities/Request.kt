898d8b08947bae5c795e334fa3f9777c8b71d66b
Artem <artemisuzl@gmail.com>
2021-12-08 23:40:14 +0300

    Поправил баги с переходами между экранами Поправил баг с проверкой полей Добавил сохранения заявки

--- app/src/main/java/com/uzlov/valitova/justcargo/model/entities/Request.kt
+++ app/src/main/java/com/uzlov/valitova/justcargo/model/entities/Request.kt
@@ -2,14 +2,15 @@ package com.uzlov.valitova.justcargo.model.entities
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import java.time.OffsetDateTime
 import java.time.OffsetTime
 
 // файл запрос на перевозку от грузоотправителя
 @Parcelize
 data class Request(
     var id: Long? = 0,
-    var requestTime: OffsetTime? = null,
-    var deliveryTime: OffsetTime? = null,
+    var requestTime: OffsetDateTime? = null,
+    var deliveryTime: OffsetDateTime? = null,
     var cost: Int? = 0,
     var departure: String? = "",
     var destination: String? = "",
--- app/src/main/java/com/uzlov/valitova/justcargo/ui/fragments/order/OrderStepOneFragment.kt
+++ app/src/main/java/com/uzlov/valitova/justcargo/ui/fragments/order/OrderStepOneFragment.kt
@@ -1,33 +1,41 @@
 package com.uzlov.valitova.justcargo.ui.fragments.order
 
 import android.app.DatePickerDialog
+import android.os.Build
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import com.uzlov.valitova.justcargo.R
 import com.uzlov.valitova.justcargo.databinding.FragmentOrderStepOneBinding
+import com.uzlov.valitova.justcargo.model.entities.Request
+import com.uzlov.valitova.justcargo.model.entities.User
+import com.uzlov.valitova.justcargo.ui.fragments.BaseFragment
+import java.time.LocalDateTime
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
 import java.util.*
 
 
-class OrderStepOneFragment : Fragment() {
-    private var _viewBinding: FragmentOrderStepOneBinding? = null
-    private val viewBinding get() = _viewBinding!!
+class OrderStepOneFragment :
+    BaseFragment<FragmentOrderStepOneBinding>(FragmentOrderStepOneBinding::inflate) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ) = FragmentOrderStepOneBinding.inflate(layoutInflater, container, false).also {
-        _viewBinding = it
-    }.root
+    private var request: Request = Request()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        (requireActivity() as AppCompatActivity).supportActionBar?.let {
+            it.title = getString(R.string.label_order_step_one)
+            it.setDisplayHomeAsUpEnabled(true)
+        }
+
+        addTextWatchers()
+        initListeners()
+    }
+
+    private fun addTextWatchers() {
         viewBinding.textInputName.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable) {}
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
@@ -49,32 +57,46 @@ class OrderStepOneFragment : Fragment() {
                 verifyEmptyEditText()
             }
         })
-        (requireActivity() as AppCompatActivity).supportActionBar?.let {
-            it.title = getString(R.string.label_order_step_one)
-            it.setDisplayHomeAsUpEnabled(true)
-        }
+        viewBinding.textDate.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                verifyEmptyEditText()
+            }
+        })
+    }
 
+    private fun initListeners() {
         viewBinding.textDate.setOnClickListener {
             openDatePicker()
         }
 
         viewBinding.buttonNextStep.setOnClickListener {
+
+            with(viewBinding) {
+                request.id = System.currentTimeMillis()
+                request.shortInfo = textInputName.text.toString()
+                request.departure = textInputFrom.text.toString()
+                request.destination = textInputTo.text.toString()
+                request.cost = 10000
+                request.owner = User(phone = "89992008289") // здесь будет браться реальная информация
+            }
+
             parentFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, OrderStepTwoFragment.newInstance())
+                .replace(R.id.fragment_container, OrderStepTwoFragment.newInstance(request))
+                .addToBackStack(null)
                 .commit()
         }
     }
 
     private fun verifyEmptyEditText() {
-        var buttonEnable = true
-        when{
-            viewBinding.textInputName.text.isNullOrEmpty() -> buttonEnable = false
-            viewBinding.textInputFrom.text.isNullOrEmpty() -> buttonEnable = false
-            viewBinding.textInputTo.text.isNullOrEmpty() -> buttonEnable = false
-            viewBinding.textDate.text.isNullOrEmpty() -> buttonEnable = false
-        }
-        viewBinding.buttonNextStep.isEnabled = buttonEnable
+        with(viewBinding) {
+            buttonNextStep.isEnabled = !textInputName.text.isNullOrEmpty() &&
+                    !textInputFrom.text.isNullOrEmpty() &&
+                    !textInputTo.text.isNullOrEmpty() &&
+                    !textDate.text.isNullOrEmpty()
 
+        }
     }
 
     private fun openDatePicker() {
@@ -85,32 +107,33 @@ class OrderStepOneFragment : Fragment() {
         val day = c.get(Calendar.DAY_OF_MONTH)
 
         val dpd =
-            context?.let {
-                DatePickerDialog(
-                    it,
-                    { _, year, monthOfYear, dayOfMonth ->
-                        viewBinding.textDate.setText(
-                            getString(
-                                R.string.for_date,
-                                dayOfMonth,
-                                (monthOfYear + 1),
-                                year
-                            )
+            DatePickerDialog(
+                requireContext(),
+                { _, year, monthOfYear, dayOfMonth ->
+                    viewBinding.textDate.setText(
+                        getString(
+                            R.string.for_date,
+                            dayOfMonth,
+                            (monthOfYear + 1),
+                            year
                         )
-                    },
-                    year,
-                    month,
-                    day
-                )
-            }
-        dpd?.datePicker?.minDate = System.currentTimeMillis() - 1000
-        dpd?.show()
+                    )
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        request.deliveryTime =
+                            OffsetDateTime.of(LocalDateTime.of(year, monthOfYear, dayOfMonth, 12, 0, 0), ZoneOffset.UTC)
+                    } else {
+                        // нужна реализация для устройств на версии < 26
+                    }
+                },
+                year,
+                month,
+                day
+            )
+        dpd.datePicker.minDate = System.currentTimeMillis() - 1000
+        dpd.show()
     }
 
-    override fun onDestroyView() {
-        _viewBinding = null
-        super.onDestroyView()
-    }
+
     companion object {
         fun newInstance() = OrderStepOneFragment().apply {
             arguments = Bundle().apply {
