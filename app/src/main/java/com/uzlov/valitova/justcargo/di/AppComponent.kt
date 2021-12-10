cddaf2232065b9c797cb0abb2c3a2a5b67a356f9
Artem <artemisuzl@gmail.com>
2021-12-11 00:58:22 +0300

    Добавил простейший сервис авторизации (эмулятор)

--- app/src/main/java/com/uzlov/valitova/justcargo/di/AppComponent.kt
+++ app/src/main/java/com/uzlov/valitova/justcargo/di/AppComponent.kt
@@ -2,10 +2,11 @@ package com.uzlov.valitova.justcargo.di
 
 
 import com.uzlov.valitova.justcargo.di.modules.*
-import com.uzlov.valitova.justcargo.ui.HostActivity
+import com.uzlov.valitova.justcargo.ui.activity.HostActivity
 import com.uzlov.valitova.justcargo.ui.fragments.FavoritesRequestsFragment
-import com.uzlov.valitova.justcargo.ui.fragments.HomeFragment
-import com.uzlov.valitova.justcargo.ui.fragments.MyDeliveriesFragment
+import com.uzlov.valitova.justcargo.ui.fragments.home.HomeSenderFragment
+import com.uzlov.valitova.justcargo.ui.fragments.profile.MyDeliveriesFragment
+import com.uzlov.valitova.justcargo.ui.fragments.profile.MyRequestsFragment
 import com.uzlov.valitova.justcargo.ui.fragments.order.OrderStepTwoFragment
 import dagger.Component
 import javax.inject.Singleton
@@ -16,6 +17,7 @@ import javax.inject.Singleton
     modules = [
         AppModule::class,
         ApiModule::class,
+        AuthService::class,
         LocalModule::class,
         RepositoriesModule::class,
         ViewModelModule::class
@@ -26,5 +28,6 @@ interface AppComponent {
     fun inject(hostActivity: MyDeliveriesFragment)
     fun inject(favoritesRequestsFragment: FavoritesRequestsFragment)
     fun inject(orderStepTwoFragment: OrderStepTwoFragment)
-    fun inject(homeFragment: HomeFragment)
+    fun inject(homeSenderFragment: HomeSenderFragment)
+    fun inject(myRequestsFragment: MyRequestsFragment)
 }
