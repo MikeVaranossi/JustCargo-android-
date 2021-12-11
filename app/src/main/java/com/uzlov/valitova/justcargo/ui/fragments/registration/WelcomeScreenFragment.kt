9a6e08956e4ce0c470b2f799f64ddb024d402fa8
Artem <artemisuzl@gmail.com>
2021-12-11 17:07:28 +0300

    add get user by ID

--- app/src/main/java/com/uzlov/valitova/justcargo/ui/fragments/registration/WelcomeScreenFragment.kt
+++ app/src/main/java/com/uzlov/valitova/justcargo/ui/fragments/registration/WelcomeScreenFragment.kt
@@ -1,28 +1,19 @@
 package com.uzlov.valitova.justcargo.ui.fragments.registration
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.util.Log
 import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.getValue
 import com.uzlov.valitova.justcargo.R
 import com.uzlov.valitova.justcargo.app.appComponent
-import com.uzlov.valitova.justcargo.databinding.FragmentRegistrationSmsBinding
 import com.uzlov.valitova.justcargo.databinding.FragmentWelcomeScreenBinding
-import com.uzlov.valitova.justcargo.model.entities.Delivery
-import com.uzlov.valitova.justcargo.model.entities.User
-import com.uzlov.valitova.justcargo.repo.usecases.RequestsUseCases
 import com.uzlov.valitova.justcargo.repo.usecases.UsersUseCase
 import com.uzlov.valitova.justcargo.ui.fragments.BaseFragment
+import com.uzlov.valitova.justcargo.viemodels.UsersViewModel
+import com.uzlov.valitova.justcargo.viemodels.ViewModelFactory
 import javax.inject.Inject
 
 
@@ -30,7 +21,9 @@ class WelcomeScreenFragment : BaseFragment<FragmentWelcomeScreenBinding>(
     FragmentWelcomeScreenBinding::inflate){
 
     @Inject
-    lateinit var usersUseCase: UsersUseCase
+    lateinit var factoryViewModel: ViewModelFactory
+    private lateinit var model: UsersViewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,16 +32,16 @@ class WelcomeScreenFragment : BaseFragment<FragmentWelcomeScreenBinding>(
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        (requireActivity() as AppCompatActivity).supportActionBar?.hide()
 
-        usersUseCase.getUsers().observe(this, {
+        model = factoryViewModel.create(UsersViewModel::class.java)
+
+        val userPhone = FirebaseAuth.getInstance().currentUser?.phoneNumber ?: ""
+        Log.e(javaClass.simpleName, "onViewCreated userPhone: $userPhone")
+        model.getUser(userPhone)?.observe(this, {
+            Log.e(javaClass.simpleName, "onViewCreated: $it")
         })
 
-        var userPhone = FirebaseAuth.getInstance().currentUser?.phoneNumber
-
-        var x = usersUseCase.getUsers(userPhone.toString())
-
-
-        (requireActivity() as AppCompatActivity).supportActionBar?.hide()
 
         val manager = requireActivity().supportFragmentManager
         viewBinding.btnLogin.setOnClickListener {
