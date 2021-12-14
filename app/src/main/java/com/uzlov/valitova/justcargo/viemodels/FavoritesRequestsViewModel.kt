4ddced56e07c1ad33814e0616a675a8b76eb933d
Artem <artemisuzl@gmail.com>
2021-12-14 23:33:58 +0300

    добавил методы в FavoritesRequestsViewModel.kt

--- app/src/main/java/com/uzlov/valitova/justcargo/viemodels/FavoritesRequestsViewModel.kt
+++ app/src/main/java/com/uzlov/valitova/justcargo/viemodels/FavoritesRequestsViewModel.kt
@@ -1,8 +1,15 @@
 package com.uzlov.valitova.justcargo.viemodels
 
+import androidx.lifecycle.LiveData
+import com.uzlov.valitova.justcargo.data.net.Request
+import com.uzlov.valitova.justcargo.repo.net.IRequestsRepository
 import javax.inject.Inject
 
 // ViewModel для взаимодествия с данными из БД (заявками добавленным ив избранное)
-class FavoritesRequestsViewModel @Inject constructor() : BaseViewModel() {
+class FavoritesRequestsViewModel @Inject constructor(var favIRequestsRepository: IRequestsRepository) : BaseViewModel() {
 
+    fun getRequests() : LiveData<List<Request>> = favIRequestsRepository.getRequests()
+    fun getRequestsByID(id: Int) : LiveData<Request?> = favIRequestsRepository.getRequest(id)
+    fun putRequest(request: Request)  = favIRequestsRepository.putRequest(request)
+    fun removeRequest(id: Int)  = favIRequestsRepository.removeRequest(id)
 }
