d499faae859eef16aad29934bb5e0165c2b76691
Artem <artemisuzl@gmail.com>
2021-12-15 00:21:08 +0300

    add inject method

--- app/src/main/java/com/uzlov/valitova/justcargo/di/AppComponent.kt
+++ app/src/main/java/com/uzlov/valitova/justcargo/di/AppComponent.kt
@@ -9,6 +9,7 @@ import com.uzlov.valitova.justcargo.ui.fragments.home.HomeSenderFragment
 import com.uzlov.valitova.justcargo.ui.fragments.profile.MyDeliveriesFragment
 import com.uzlov.valitova.justcargo.ui.fragments.profile.MyRequestsFragment
 import com.uzlov.valitova.justcargo.ui.fragments.order.OrderStepTwoFragment
+import com.uzlov.valitova.justcargo.ui.fragments.profile.PersonalDataFragment
 import com.uzlov.valitova.justcargo.ui.fragments.registration.RegistrationSmsFragment
 import com.uzlov.valitova.justcargo.ui.fragments.registration.WelcomeScreenFragment
 import dagger.Component
@@ -36,4 +37,5 @@ interface AppComponent {
     fun inject(myRequestsFragment: MyRequestsFragment)
     fun inject(authService: AuthService)
     fun inject(registrationSmsFragment: RegistrationSmsFragment)
+    fun inject(personalDataFragment: PersonalDataFragment)
 }
