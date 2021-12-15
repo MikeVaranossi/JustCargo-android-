a2c9dad3bf742ead044f17f1e5f6dd00da6d778b
Yulia Valitova <ikskulfg@gmail.com>
2021-12-15 03:03:09 +0300

    фикс конфликтов

--- app/src/main/java/com/uzlov/valitova/justcargo/ui/fragments/HomeCarrierFragment.kt
+++ app/src/main/java/com/uzlov/valitova/justcargo/ui/fragments/HomeCarrierFragment.kt
@@ -1,126 +1,66 @@
 package com.uzlov.valitova.justcargo.ui.fragments
 
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.PopupMenu
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.uzlov.valitova.justcargo.R
-import com.uzlov.valitova.justcargo.app.appComponent
-import com.uzlov.valitova.justcargo.data.net.Request
 import com.uzlov.valitova.justcargo.databinding.FragmentHomeCarrierBinding
-import com.uzlov.valitova.justcargo.viemodels.RequestsViewModel
-import javax.inject.Inject
 
 
-class HomeCarrierFragment : BaseFragment<FragmentHomeCarrierBinding>(
-    FragmentHomeCarrierBinding::inflate
-) {
-
-    @Inject
-    lateinit var viewModelFactory: ViewModelProvider.Factory
-    private lateinit var model: RequestsViewModel
+class HomeCarrierFragment : Fragment() {
+    private var _viewBinding: FragmentHomeCarrierBinding? = null
+    private val viewBinding get() = _viewBinding!!
     private val adapter = RVHomeCarrierAdapter()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        requireContext().appComponent.inject(this)
-        super.onCreate(savedInstanceState)
-        model = viewModelFactory.create(RequestsViewModel::class.java)
-        setHasOptionsMenu(true)
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.toolbar_menu, menu)
-        super.onCreateOptionsMenu(menu, inflater)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.bell_action -> Toast.makeText(
-                context,
-                "Здесь будут доступные уведомления",
-                Toast.LENGTH_SHORT
-            ).show()
-            R.id.share_action -> Toast.makeText(
-                context,
-                "Здесь можно будет поделиться с кем-нибудь непонятно чем",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-        return true
-    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = FragmentHomeCarrierBinding.inflate(layoutInflater, container, false).also {
+        _viewBinding = it
+    }.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         (requireActivity() as AppCompatActivity).supportActionBar?.let {
-            it.title = getString(R.string.label_home_carrier_fragment)
+            it.title = getString(R.string.app_name)
             it.setDisplayHomeAsUpEnabled(false)
 
         }
         viewBinding.recyclerViewHomeCarrier.adapter = adapter
-        loadData()
-        viewBinding.buttonSort.setOnClickListener {
-            showPopUp(it)
-        }
         viewBinding.buttonFindCargo.setOnClickListener {
             parentFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, SearchFragment.newInstance())
                 .commit()
         }
+        checkRV()
 
     }
 
-    private fun loadData() {
-        model.getRequests()?.observe(this, {
-            checkRV(it)
-        })
+    override fun onDestroyView() {
+        _viewBinding = null
+        super.onDestroyView()
     }
 
-    private fun checkRV(data: List<Request>) {
-        adapter.setData(data)
-        if (data.isEmpty()) {
+    private fun checkRV() {
+        if (adapter.itemCount == 0) {
             viewBinding.imageViewCarrierHomeEmpty.visibility = View.VISIBLE
             viewBinding.texViewLabelEmpty.visibility = View.VISIBLE
             viewBinding.recyclerViewHomeCarrier.visibility = View.GONE
             viewBinding.buttonFindCargo.visibility = View.GONE
-            viewBinding.textViewFindCargo.visibility = View.GONE
             loadImage(R.drawable.image_home_carrier_fragment, viewBinding.imageViewCarrierHomeEmpty)
         } else {
             viewBinding.imageViewCarrierHomeEmpty.visibility = View.GONE
             viewBinding.texViewLabelEmpty.visibility = View.GONE
             viewBinding.recyclerViewHomeCarrier.visibility = View.VISIBLE
             viewBinding.buttonFindCargo.visibility = View.VISIBLE
-            viewBinding.textViewFindCargo.text =
-                getString(R.string.text_label_find_cargo, data.size)
-        }
-    }
-
-    private fun showPopUp(view: View) {
-        val wrapper = ContextThemeWrapper(view.context, R.style.MyStyle_PopupMenu)
-        val popupMenu = context?.let { PopupMenu(wrapper, view) }
-        val inflater = popupMenu?.menuInflater
-        inflater?.inflate(R.menu.sort_popup_menu, popupMenu.menu)
-        popupMenu?.show()
-        popupMenu?.setOnMenuItemClickListener {
-            when (it.itemId) {
-                R.id.item1 -> {
-                    Toast.makeText(view.context, it.title, Toast.LENGTH_SHORT).show()
-                }
-                R.id.item2 -> {
-                    Toast.makeText(view.context, it.title, Toast.LENGTH_SHORT).show()
-                }
-                R.id.item3 -> {
-                    Toast.makeText(view.context, it.title, Toast.LENGTH_SHORT).show()
-                }
-                R.id.item4 -> {
-                    Toast.makeText(view.context, it.title, Toast.LENGTH_SHORT).show()
-                }
-            }
-            true
         }
     }
 
--- app/src/main/java/com/uzlov/valitova/justcargo/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/uzlov/valitova/justcargo/ui/fragments/HomeFragment.kt
@@ -3,32 +3,18 @@ package com.uzlov.valitova.justcargo.ui.fragments
 import android.os.Bundle
 import android.view.*
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.uzlov.valitova.justcargo.R
+import com.uzlov.valitova.justcargo.app.appComponent
+import com.uzlov.valitova.justcargo.data.net.Request
 import com.uzlov.valitova.justcargo.databinding.FragmentHomeBinding
+import com.uzlov.valitova.justcargo.repo.usecases.RequestsUseCases
 import com.uzlov.valitova.justcargo.ui.fragments.order.OrderStepOneFragment
+import javax.inject.Inject
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setHasOptionsMenu(true)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.toolbar_menu, menu)
-        menu.setGroupVisible(R.id.group2, false)
-        super.onCreateOptionsMenu(menu, inflater)
-    }
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
-            R.id.bell_action -> Toast.makeText(context, "Здесь будут доступные уведомления", Toast.LENGTH_SHORT).show()
-        }
-        return true
-    }
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
